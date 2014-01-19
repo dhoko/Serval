97dc31402ae07183e927c4d14ec7b00541cb8c82
dhoko <aurelien@procheo.fr>
2014-01-19 19:36:06 +0100

    Move vendor

---
+++ src/js/bootsrap.js
@@ -0,0 +1,13 @@
+(function(win, doc, App){
+
+  App.Models = {};
+  App.Models.Instances = {};
+  App.Collections = {};
+  App.Collections.Instances = {};
+  App.Views = {};
+  App.Views.Instances = {};
+  App.Routers = {};
+  App.Routers.Instances = {};
+  App.Events = {};
+
+})(window, window.document, window.app || (window.app = {}));
